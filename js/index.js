549fbdc4412435af672c4c2f7914faed393739b3
Victoria Caracchi <vickypau1d@gmail.com>
2025-07-15 21:10:27 -0300

    agregue tarjetas de productos

---
+++ js/index.js
@@ -0,0 +1,17 @@
+const contenedorTarjetas = document.getElementById("productos-container");
+
+
+function mostrarProductos(prods){
+  prods.forEach(producto => {
+    const nuevoProd = document.createElement("div");
+    nuevoProd.classList = "tarjeta-producto"
+    nuevoProd.innerHTML = `
+    <img src="${producto.img}" alt="${producto.nombre}">
+    <h3>${producto.nombre}</h3>
+    <p class="precio">$${producto.precio}</p>
+    <button>Agregar al carrito</button>`
+    contenedorTarjetas.appendChild(nuevoProd);
+    /*nuevoProd.getElementsByTagName("button")[0].addEventListener("click",() => agregarAlCarrito(producto))*/
+  });
+}
+mostrarProductos(productos);
---
+++ js/productos.js
@@ -0,0 +1,45 @@
+/* archivo de prueba pagina sin backend */
+const productos = [
+  {
+    id:1,
+    nombre: "Bombe",
+    img: "../img/bombe.png",
+    precio: 11100,
+    peso:310,
+  },
+    {
+    id:2,
+    nombre: "Chiquitin",
+    img: "../img/chiquitin.png",
+    precio: 2900,
+    peso:42,
+  },
+    {
+    id:3,
+    nombre: "Lata",
+    img: "../img/lata.png",
+    precio: 4400,
+    peso:90,
+  },
+    {
+    id:4,
+    nombre: "Lolo",
+    img: "../img/lolo.png",
+    precio: 4500,
+    peso:110,
+  },
+    {
+    id:5,
+    nombre: "Moon",
+    img: "../img/moon.png",
+    precio: 4600,
+    peso:105,
+  },
+    {
+    id:6,
+    nombre: "Whisky sin tapa",
+    img: "../img/whisky.png",
+    precio: 6900,
+    peso:190,
+  }
+];
