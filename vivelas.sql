ac709eadd603364c22696adcbc9993451cb6658d
Enrico Reschini <ereschini06@gmail.com>
2025-10-14 15:52:30 -0300

    actualice el documento con la base de datos

--- vivelas.sql
+++ vivelas.sql
@@ -24,13 +24,13 @@ DROP TABLE IF EXISTS `clientes`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `clientes` (
   `idCli` int NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(100) NOT NULL,
+  `nombreCli` varchar(100) NOT NULL,
   `apellido` varchar(100) DEFAULT NULL,
   `direccion` varchar(200) DEFAULT NULL,
   `email` varchar(100) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `idTipoCli` int DEFAULT '1',
   `creado_en` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `idTipoCli` int DEFAULT '2',
   PRIMARY KEY (`idCli`),
   UNIQUE KEY `email` (`email`),
   KEY `idTipoCli` (`idTipoCli`),
@@ -44,36 +44,8 @@ CREATE TABLE `clientes` (
 
 LOCK TABLES `clientes` WRITE;
 /*!40000 ALTER TABLE `clientes` DISABLE KEYS */;
-INSERT INTO `clientes` VALUES (1,'lauti','ponce','amenabar','lauti@gmail.com','$2b$10$FwgRBHhMpMkci9wVM3YX3Omg3NIUJa/Kc1w3zpfWV9exNnFqAk7Ba',1,'2025-10-10 14:15:55'),(2,'el pepe ','jaja','9dejulio','lauti03@gmail.com','$2b$10$vqsAtQpHQWLeBEBKqaJiK.mW7C2qWASumtYiAJ5jst3ByQITpGt2m',1,'2025-10-10 14:45:08'),(3,'aaa','aaa','aaa','aaa@gmail.com','$2b$10$yINt.xYriHanb6tb83qKCOtARqCSsBxlGjEqw2OEa6tOwmz3Hxlhm',1,'2025-10-10 15:34:17'),(4,'pepe','pepe','pepe','pepe@gmail.com','$2b$10$D.G/ajWDF07sw6.S/HGkLecDFCKt/aHjH3QwoAVR8cCF6dT2951e.',1,'2025-10-10 18:02:19');
+INSERT INTO `clientes` VALUES (1,'lauti','ponce','amenabar','lauti@gmail.com','$2b$10$FwgRBHhMpMkci9wVM3YX3Omg3NIUJa/Kc1w3zpfWV9exNnFqAk7Ba','2025-10-10 14:15:55',1),(2,'el pepe ','jaja','9dejulio','lauti03@gmail.com','$2b$10$vqsAtQpHQWLeBEBKqaJiK.mW7C2qWASumtYiAJ5jst3ByQITpGt2m','2025-10-10 14:45:08',2),(3,'aaa','aaa','aaa','aaa@gmail.com','$2b$10$yINt.xYriHanb6tb83qKCOtARqCSsBxlGjEqw2OEa6tOwmz3Hxlhm','2025-10-10 15:34:17',2),(4,'pepe','pepe','pepe','pepe@gmail.com','$2b$10$D.G/ajWDF07sw6.S/HGkLecDFCKt/aHjH3QwoAVR8cCF6dT2951e.','2025-10-10 18:02:19',2);
 /*!40000 ALTER TABLE `clientes` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `productos`
---
-
-DROP TABLE IF EXISTS `productos`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `productos` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(100) NOT NULL,
-  `precio` decimal(10,2) NOT NULL,
-  `urlImg` varchar(256) DEFAULT NULL,
-  `deleted` tinyint DEFAULT NULL,
-  `medida` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `productos`
---
-
-LOCK TABLES `productos` WRITE;
-/*!40000 ALTER TABLE `productos` DISABLE KEYS */;
-INSERT INTO `productos` VALUES (1,'Bombe',11100.00,'https://i.postimg.cc/Wzcb5NtJ/bombe.png',0,'310'),(2,'Chiquitin',2900.00,'https://i.postimg.cc/tTx4dZRj/chiquitin.png',0,'42'),(3,'Lata',4400.00,'https://i.postimg.cc/MH0Z4Cj3/lata.png',0,'90'),(4,'Lolo',4500.00,'https://i.postimg.cc/pLLVG1Vk/lolo.png',0,'110'),(5,'Moon',4600.00,'https://i.postimg.cc/Nf4fxwt5/moon.png',0,'105'),(6,'Whisky sin tapa',6900.00,'https://i.postimg.cc/kgYJkFBC/whisky.png',0,'190'),(16,'nuevo',200.00,'https://i.postimg.cc/kgYJkFBC/whisky.png',0,'100'),(17,'otro',200.00,'https://i.postimg.cc/kgYJkFBC/whisky.png',0,'100'),(18,'prueba',200.00,'https://i.postimg.cc/kgYJkFBC/whisky.png',NULL,'100'),(19,'hola',200.00,'https://i.postimg.cc/kgYJkFBC/whisky.png',NULL,'100'),(20,'moon',1500.00,'https://i.postimg.cc/kgYJkFBC/whisky.png',NULL,'300'),(21,'aa',32.03,NULL,NULL,'aaa');
-/*!40000 ALTER TABLE `productos` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -88,7 +60,7 @@ CREATE TABLE `tipo_clientes` (
   `nombreTipo` varchar(50) NOT NULL,
   `descuento` decimal(5,2) DEFAULT '0.00',
   PRIMARY KEY (`idTipoCli`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -97,40 +69,81 @@ CREATE TABLE `tipo_clientes` (
 
 LOCK TABLES `tipo_clientes` WRITE;
 /*!40000 ALTER TABLE `tipo_clientes` DISABLE KEYS */;
-INSERT INTO `tipo_clientes` VALUES (1,'Inicial',0.00),(2,'Medium',5.00),(3,'Premium',10.00);
+INSERT INTO `tipo_clientes` VALUES (1,'Admin',0.00),(2,'Inicial','0.00'),(3,'Medium',5.00),(4,'Premium',10.00);
 /*!40000 ALTER TABLE `tipo_clientes` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `productos`
+--
+
+DROP TABLE IF EXISTS `productos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `productos` (
+  `idProd` int NOT NULL AUTO_INCREMENT,
+  `nombreProd` varchar(100) NOT NULL,
+  `precioProd` decimal(10,2) NOT NULL,
+  `urlImg` varchar(256) DEFAULT NULL,
+  `deleted` tinyint DEFAULT NULL,
+  `medida` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idProd`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `productos`
+--
+
+LOCK TABLES `productos` WRITE;
+/*!40000 ALTER TABLE `productos` DISABLE KEYS */;
+INSERT INTO `productos` VALUES (1,'Bombe',11100.00,'https://i.postimg.cc/Wzcb5NtJ/bombe.png',0,'310'),(2,'Chiquitin',2900.00,'https://i.postimg.cc/tTx4dZRj/chiquitin.png',0,'42'),(3,'Lata',4400.00,'https://i.postimg.cc/MH0Z4Cj3/lata.png',0,'90'),(4,'Lolo',4500.00,'https://i.postimg.cc/pLLVG1Vk/lolo.png',0,'110'),(5,'Moon',4600.00,'https://i.postimg.cc/Nf4fxwt5/moon.png',0,'105'),(6,'Whisky sin tapa',6900.00,'https://i.postimg.cc/kgYJkFBC/whisky.png',0,'190'),(7,'otro',200.00,'https://i.postimg.cc/kgYJkFBC/whisky.png',0,'100'),(8,'prueba',200.00,'https://i.postimg.cc/kgYJkFBC/whisky.png',NULL,'100'),(9,'hola',200.00,'https://i.postimg.cc/kgYJkFBC/whisky.png',NULL,'100'),(10,'moon',1500.00,'https://i.postimg.cc/kgYJkFBC/whisky.png',NULL,'300'),(11,'nuevo',200.00,'https://i.postimg.cc/kgYJkFBC/whisky.png',0,'100'),(12,'aa',32.03,NULL,NULL,'aaa');
+/*!40000 ALTER TABLE `productos` ENABLE KEYS */;
+UNLOCK TABLES;
+
 
 DROP TABLE IF EXISTS `descuentos`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `descuentos` (
-  `idDesc` INT NOT NULL AUTO_INCREMENT,
-  `porcentaje` DECIMAL(5,2) NOT NULL, -- Valor de porcentaje, e.g., 20.00
+CREATE TABLE descuentos (
+  `idDesc` int NOT NULL AUTO_INCREMENT,
+  `porcentaje` DECIMAL(5,2) NOT NULL,
   `fechaDesde` DATE NOT NULL,
-  `fechaHasta` DATE DEFAULT NULL,
+  `fechaHasta` DATE NOT NULL,
   PRIMARY KEY (`idDesc`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `descuentos`
+--
+
+LOCK TABLES `descuentos` WRITE;
+/*!40000 ALTER TABLE `descuentos` DISABLE KEYS */;
+INSERT INTO `descuentos` VALUES (1,10.00,'2025-10-14','2026-01-01'),(2,15.00,'2025-10-14','2026-01-01'),(3,20.00,'2025-10-14','2026-01-01'),(4,25.00,'2025-10-14','2026-01-01');
+/*!40000 ALTER TABLE `descuentos` ENABLE KEYS */;
+UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `productos_descuentos`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `productos_descuentos` (
-  `idDesc` INT NOT NULL,
   `idProd` INT NOT NULL,
-  PRIMARY KEY (`idDesc`, `idProd`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `idDesc` INT NOT NULL,
+  KEY `idProd` (`idProd`),
+  KEY `idDesc` (`idDesc`),
+  PRIMARY KEY (`idProd`, `idDesc`),
+  CONSTRAINT `productos_descuentos_ibfk_1` FOREIGN KEY (`idProd`) REFERENCES `productos` (`idProd`),
+  CONSTRAINT `productos_descuentos_ibfk_2` FOREIGN KEY (`idDesc`) REFERENCES `descuentos` (`idDesc`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+--
+-- Dumping data for table `descuentos`
+--
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2025-10-10 16:05:23
+LOCK TABLES `productos_descuentos` WRITE;
+/*!40000 ALTER TABLE `productos_descuentos` DISABLE KEYS */;
+INSERT INTO `productos_descuentos` VALUES (1,1),(2,1),(3,2);
+/*!40000 ALTER TABLE `productos_descuentos` ENABLE KEYS */;
+UNLOCK TABLES;
